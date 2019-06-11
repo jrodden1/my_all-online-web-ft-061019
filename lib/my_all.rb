bc57819c30e764efda0fea76ce1d93118ce55197
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-11 17:20:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,13 @@ require 'pry'
 
 def my_all?(collection)
   if collection != []
+    i = 0
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
     binding.pry
   else
-    #specify a collection 
+    #specify a collection
   end
 end
